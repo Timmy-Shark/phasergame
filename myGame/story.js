48ce33e86ddae8e5e17c16816bd7df2f47d49e71
Timmy Singer <tsinger.sh@mysummitps.org>
2016-11-09 20:11:44 +0000

    update

--- myGame/story.js
+++ myGame/story.js
@@ -1,16 +1,26 @@
 /* global game phaser game_state */
-game_state.story + function() {};
+game_state.story = function() {};
 
 game_state.story.prototype = {
 
-    preload: function(){
-    
+    preload: function() {
+
     },
-    create: function(){
+    create: function() {
+        // The this.score
+        this.scoreText = game.add.text(16, 16, 'This is the story about the lizard. \n He is trying to catch all the stars there are!', {
+            fontSize: '32px',
+            fill: '#ffffff'
+        });
+
     
+    
+
+
+
     },
-    update: function(){
-    
+    update: function() {
+
     }
 }
 
